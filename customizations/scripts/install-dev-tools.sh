d5b43f7af41b350b407c91703dc393cdb2c417ab
Marcos Pereira <marcos.silva@gmail.com>
2023-06-08 21:43:32 -0400

    Script to install a bunch of utilities

---
+++ customizations/scripts/install-dev-tools.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+brew install --display-times git
+brew install --display-times gh
+
+brew install --display-times direnv
+brew install --display-times ripgrep
+brew install --display-times htop
+brew install --display-times tree
+brew install --display-times watch
+brew install --display-times jq
+brew install --display-times yq
+
+brew install --display-times wget
+brew install --display-times httpie
+brew install --display-times geckodriver
+brew install --display-times --cask chromedriver
