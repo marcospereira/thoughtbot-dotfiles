8c39761aee924dc5c33d8f4f93055fc41c643db3
Marcos Pereira <marcos.silva@gmail.com>
2023-06-08 21:49:12 -0400

    Copy customization files to `~/dotfiles-local`

---
+++ customizations/scripts/copy-customizations.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p "$HOME/dotfiles-local"
+
+# It is relevant to keep the trailing slash in the source path to
+# avoid creating an additional directory level at the destination.
+#
+# The --archive option ensures that symbolic links, devices, attributes,
+# permissions, ownerships, etc. are preserved in the transfer. It will
+# also copy recursively.
+rsync --verbose --archive --human-readable --progress "$PWD/customizations/dotfiles-local/" "$HOME/dotfiles-local"
