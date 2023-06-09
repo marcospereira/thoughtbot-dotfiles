4d6e6fe0b1453708dfb5ef0feba655490baa0027
Marcos Pereira <marcos.silva@gmail.com>
2023-06-08 21:46:13 -0400

    Script to install a bunch of DMGs

---
+++ customizations/scripts/install-dmgs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+brew update
+
+brew install --cask google-chrome
+brew install --cask google-drive
+brew install --cask firefox
+brew install --cask spotify
+brew install --cask vlc
+brew install --cask dropbox
+brew install --cask iterm2
+brew install --cask github
+brew install --cask raycast
+brew install --cask the-unarchiver
+brew install --cask adobe-acrobat-reader
+brew install --cask intellij-idea
+brew install --cask visual-studio-code
+brew install --cask grammarly
+brew install --cask nordvpn
+brew install --cask rectangle
+
+brew install --cask gpg-suite
